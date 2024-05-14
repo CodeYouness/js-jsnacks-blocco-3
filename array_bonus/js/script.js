649eb5602edc9ff9190bdea48bf3178775e22be0
CodeYouness <ylijassi@gmail.com>
2024-05-14 17:45:04 +0200

    fixed uppercase of math.abs

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -1,7 +1,7 @@
 const firstArray = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
 const secondArray = ['pesca', 'cocomero']
 
-for (let i = 0; i < (math.abs((firstArray.lenght) - (secondArray.length))); i++) {
+for (let i = 0; i < (Math.abs((firstArray.lenght) - (secondArray.length))); i++) {
     if (firstArray.length < secondArray.lenght) {
 
     } else if (firstArray.length > secondArray.lenght) {
