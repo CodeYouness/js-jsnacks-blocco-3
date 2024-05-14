a57859b6c480b6a668eb420679984cdafaffc0bd
CodeYouness <ylijassi@gmail.com>
2024-05-14 18:07:59 +0200

    added if to push an element inside one of the two array

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -1,10 +1,14 @@
 const firstArray = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
-const secondArray = ['pesca', 'cocomero']
+const secondArray = ['pesca', 'cocomero'];
+const comparationLenght = Math.abs(firstArray.length - secondArray.length);
 
-for (let i = 0; i < (Math.abs((firstArray.lenght) - (secondArray.length))); i++) {
-    if (firstArray.length < secondArray.lenght) {
-
-    } else if (firstArray.length > secondArray.lenght) {
-
+for (let i = 0; i < comparationLenght; i++) {
+    if (firstArray.length < secondArray.length) {
+        firstArray.push('sugo')
+    } else if (firstArray.length > secondArray.length) {
+        secondArray.push('sughissimo')
     }
+
 }
+
+console.log(firstArray, firstArray.length, secondArray, secondArray.length)
