9c351e165c1283be7f911f6c17c16d4494a5c655
CodeYouness <ylijassi@gmail.com>
2024-05-14 17:42:18 +0200

    added 2 const with different number of element added for cycle that repeat for the number of difference between the two array

---
+++ array_bonus/js/script.js
@@ -0,0 +1,6 @@
+const firstArray = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
+const secondArray = ['pesca', 'cocomero']
+
+for (let i = 0; i < (math.abs((firstArray.lenght) - (secondArray.length))); i++) {
+
+}
