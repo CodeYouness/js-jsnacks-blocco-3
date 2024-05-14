facf02dc5b90e136ee85c452a8fbde9aa1e1ba2f
CodeYouness <ylijassi@gmail.com>
2024-05-14 17:13:45 +0200

    added for (repeat the process for the lenght of fruit container) added if (check if there is 'cocomero' inside of fruit container

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,3 +1,13 @@
 const fruit = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola']
 fruit.push('pesca')
-console.log(fruit)
+
+for (let i = 0; i <= fruit.length; i++) {
+
+    if (fruit[i] === 'cocomero') {
+        console.log("Trovato! Devo solo preparare il cocktail.")
+        break;
+    } else if (i = fruit.length) {
+        console.log("Oh no, devo uscire a comprare il cocomero!", i)
+    }
+
+}
