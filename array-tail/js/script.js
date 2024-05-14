3ddff536535bea30d466165158391e62d1699f71
CodeYouness <ylijassi@gmail.com>
2024-05-14 17:32:32 +0200

    added if to check whats the number of cycle done (to add the last number on another container to show to user)

--- array-tail/js/script.js
+++ array-tail/js/script.js
@@ -1,9 +1,13 @@
-const userNumber = Number.parseInt(prompt('give me a number'));
+const userNumber = Number.parseInt(prompt('how many number to generate inside the array?'));
+const lastUserNumber = Number.parseInt(prompt('how many numbers you want to see of your array?'));
 const numberContainer = []
-
+const lastFiveNumber = []
 for (let i = 0; i < userNumber; i++) {
     const casualNumber = Math.floor(Math.random() * 100) + 1;
-    numberContainer.push(casualNumber)
+    numberContainer.push(casualNumber);
+    if ((userNumber - i) <= lastUserNumber) {
+        lastFiveNumber.push(casualNumber)
+    }
 }
 
-console.log(numberContainer)
+console.log(numberContainer, lastFiveNumber)
