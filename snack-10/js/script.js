bcbf06980c5d9301c3c9e6e74a50b3ae42124bce
CodeYouness <ylijassi@gmail.com>
2024-05-15 12:15:24 +0200

    added array

---
+++ snack-10/js/script.js
@@ -0,0 +1,6 @@
+//Crea due array che hanno un numero di elementi diversi. 
+//Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
+
+const shortArray = [1, 2, 3]
+const longhArray = [1, 2, 3, 4, 5, 6]
+
