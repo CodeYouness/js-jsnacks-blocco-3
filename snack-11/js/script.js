f9c18b79a20bd41a8ddf9b571eb512cc0a993f03
CodeYouness <ylijassi@gmail.com>
2024-05-15 12:45:37 +0200

    first iteration of excercise, without control of the number

---
+++ snack-11/js/script.js
@@ -0,0 +1,12 @@
+//Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+//Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+const arrayNumbers = []
+let sum = 0
+
+while (sum < 50) {
+    let userNumber = Number.parseInt(prompt('type a number'), 10);
+    arrayNumbers.push(userNumber);
+    sum += userNumber;
+}
+console.log(sum, arrayNumbers)
