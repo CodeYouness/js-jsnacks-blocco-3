31bea0f61e72f7326b6e6445db2047d5f7419f0a
CodeYouness <ylijassi@gmail.com>
2024-05-15 12:25:27 +0200

    first implementation of excercise

--- snack-10/js/script.js
+++ snack-10/js/script.js
@@ -1,6 +1,24 @@
 //Crea due array che hanno un numero di elementi diversi. 
 //Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
-const shortArray = [1, 2, 3]
-const longhArray = [1, 2, 3, 4, 5, 6]
+const array1 = [1, 2, 3]
+const array2 = [1, 2, 3, 4, 5, 6]
+let arrayLong
+let arrayShort
 
+if (array1.length < array2.length) {
+    arrayShort = array1
+    arrayLong = array2
+} else {
+    arrayLong = array1
+    arrayShort = array2
+}
+
+console.log(array1, array2, arrayShort, arrayLong)
+
+while (arrayShort < arrayLong) {
+    let casualNumber = Math.floor(Math.random() * 10) + 1;
+    arrayShort.push(casualNumber)
+}
+
+console.log(arrayShort, arrayLong)
