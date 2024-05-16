e96b44673dd24d96ede7e5587c77e966bfd43576
CodeYouness <ylijassi@gmail.com>
2024-05-16 12:24:46 +0200

    removed for cycle

--- snack-13/js/script.js
+++ snack-13/js/script.js
@@ -1,12 +1,9 @@
 // Scrivi una funzione che dato un numero inverta le sue cifre: 1234 -> 4321
 
 function invertNumber(number) {
-    let invertedNumber = []
-    let numberString = number.toString();
-    for (let i = 0; i < numberString.length; i++) {
-        invertedNumber.push(numberString[i])
-        console.log(numberString[i])
-    }
+
+
+    let invertedNumber = number.reverse()
     return invertedNumber
 }
 
