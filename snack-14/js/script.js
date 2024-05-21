61491b55bde991d4dadf2d14e2f6928ce1bfce7d
CodeYouness <ylijassi@gmail.com>
2024-05-21 12:40:55 +0200

    fixed buf

--- snack-14/js/script.js
+++ snack-14/js/script.js
@@ -1,14 +1,15 @@
 //Chiedi all'utente un numero di secondi da attendere
 //e fai partire un timer da quel momento a zero secondi, interrompendo il timer
 
-const userSecond = Number.parseInt(prompt('define your timer'), 10)
+const userSecond = Number.parseInt(prompt('define your timer'), 10);
+let remainingTime = userSecond
 
 const startTimer = setInterval(timer, 1000, userSecond);
 
-function timer(usertime) {
-    usertime--;
-    console.log(usertime)
-    if (usertime === 0) {
+function timer() {
+    remainingTime--;
+    console.log(remainingTime)
+    if (remainingTime === 0) {
         clearInterval(startTimer)
     }
 }
