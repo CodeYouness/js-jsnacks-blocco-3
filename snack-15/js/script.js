cf651facaf1033f9021c64e2bb9643a56263c009
CodeYouness <ylijassi@gmail.com>
2024-05-22 12:44:23 +0200

    fixed problem inside object

--- snack-15/js/script.js
+++ snack-15/js/script.js
@@ -6,12 +6,12 @@ Attraverso un prompt dare la possibilità all'utente di modificare il peso della
 */
 
 let palla = {
-    ballName: palla,
+    ballName: 'palla',
     weight: 10
 }
 
 const userWeight = prompt('what\s the weight of the ball?');
 
-palla.weight = number(userWeight);
+palla.weight = Number(userWeight);
 
 console.log(palla)
