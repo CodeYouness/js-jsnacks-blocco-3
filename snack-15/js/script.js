d7ab8c05ca8b8aeb202af93b7eb35d11bfbac276
CodeYouness <ylijassi@gmail.com>
2024-05-22 12:38:35 +0200

    added array and prompt

---
+++ snack-15/js/script.js
@@ -0,0 +1,13 @@
+/*
+Creare un oggetto palla che abbia le seguenti proprietà.
+Nome = palla
+Peso = 10
+Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla.
+*/
+
+let palla = {
+    ballName: palla,
+    weight: 10
+}
+
+const userWeight = prompt('what\s the weight of the ball?')
