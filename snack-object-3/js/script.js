5859b8b2a52d0a9981c921e47199bd9d95657b33
CodeYouness <ylijassi@gmail.com>
2024-05-23 12:34:55 +0200

    added array

---
+++ snack-object-3/js/script.js
@@ -0,0 +1,55 @@
+// Crea un array di oggetti che rappresentano delle persone, 10.
+// Ogni persona ha un nome, un cognome e un'età.
+
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+
+const family = [{
+    nameFamily: 'marco',
+    surName: 'marchino',
+    years: 13
+},
+{
+    nameFamily: 'ilaria',
+    surName: 'larietta',
+    years: 29
+},
+{
+    nameFamily: 'francesco',
+    surName: 'de franceschi',
+    years: 33
+},
+{
+    nameFamily: 'germano',
+    surName: 'milite',
+    years: 11
+},
+{
+    nameFamily: 'me',
+    surName: 'meStesso',
+    years: 8
+},
+{
+    nameFamily: 'giovanni',
+    surName: 'giovinis',
+    years: 49
+},
+{
+    nameFamily: 'antonio',
+    surName: 'dehpeffo',
+    years: 66
+},
+{
+    nameFamily: 'dematteo',
+    surName: 'mattei',
+    years: 77
+},
+{
+    nameFamily: 'blio',
+    surName: 'oblio',
+    years: 3
+},
+{
+    nameFamily: 'dario',
+    surName: 'moccia',
+    years: 100
+},]
